09fd72ce52fa1a47f880fe21ba73ae44eb40c444
KBRANUBIS <gptpro3.3@gmail.com>
2025-07-24 16:35:18 +0300

    Update babel.config.js after removing react-native-worklets

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,19 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-worklets/plugin', // Новый плагин для Reanimated
+      'react-native-reanimated/plugin', // Оставь, если используешь Reanimated
+      '@babel/plugin-proposal-export-namespace-from',
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './',
+          },
+        },
+      ],
     ],
   };
 };
