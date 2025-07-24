9fa3e219c92cc2f95a98edf1a3ceae737d661981
KBRANUBIS <gptpro3.3@gmail.com>
2025-07-24 14:29:52 +0300

    Add EAS project ID to app.config.js

--- app.config.js
+++ app.config.js
@@ -3,7 +3,12 @@ module.exports = {
     name: "Collection",
     version: "1.0.0",
     android: {
-      newArchEnabled: true, // Включаем новую архитектуру
+      newArchEnabled: true,
+    },
+    extra: {
+      eas: {
+        projectId: "e77a3e97-38d4-4020-9ee5-686bef93ff9f",
+      },
     },
   },
 };
